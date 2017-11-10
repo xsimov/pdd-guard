aafbeb053eb634438b6d8d7dc496da6113da2846
Xavier Simó <xsimov@gmail.com>
2017-11-10 21:29:18 +0100

    Extract parseTimestamp function and add 5s sleep to infinite loop

--- main.go
+++ main.go
@@ -10,28 +10,36 @@ import (
 
 func main() {
 	go checkLastCall()
-	http.HandleFunc("/pdd", savePddCall)
-	log.Fatalf("ERROR!", http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/pdd", writeTimestampToDisk)
+	log.Fatalf("error: %v", http.ListenAndServe(":8080", nil))
 }
 
 const (
-	fPath = "pdd.db"
+	fPath      = "pdd.db"
+	timeLayout = time.RFC3339
 )
 
-func savePddCall(w http.ResponseWriter, r *http.Request) {
-	timeStr := []byte(fmt.Sprintf("%v", time.Now().Format(time.RFC3339)))
+func writeTimestampToDisk(w http.ResponseWriter, r *http.Request) {
+	timeStr := []byte(fmt.Sprintf("%v", time.Now().Format(timeLayout)))
 	ioutil.WriteFile(fPath, timeStr, 0600)
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Created!"))
 }
 
 func checkLastCall() {
-	fContent, _ := ioutil.ReadFile(fPath)
-	t, err := time.Parse(time.RFC3339, string(fContent))
+	fContent, err := ioutil.ReadFile(fPath)
+	if err == nil {
+		parseTimestamp(fContent)
+	}
+	time.Sleep(5 * time.Second)
+	checkLastCall()
+}
+
+func parseTimestamp(content []byte) {
+	t, err := time.Parse(timeLayout, string(content))
 	if err == nil {
 		if time.Since(t) > 30*time.Second {
-			fmt.Println("Boum! Last time was: ", t)
+			fmt.Println("It's been a long time since last call! 30 seconds!", t)
 		}
 	}
-	checkLastCall()
 }
